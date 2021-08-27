c30c92b09bfc96fe4bcb7778d350bcb29916dadf
paola9896 <paolasc2652@gmail.com>
2021-08-27 14:18:30 +0000

    Changes to app.js, style.css, and index.html

--- src/app.js
+++ src/app.js
@@ -2,10 +2,20 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //Arrays with their values
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big", "amazing", "cool"];
+  let noun = ["jogger", "racoon", "princess", "king", "friend"];
+  let extension = [".com", ".net", ".us", ".io", "lastOf.us"];
+
+  //Random numbers for each array and their values
+  let random1 = Math.floor(Math.random() * pronoun.length);
+  let random2 = Math.floor(Math.random() * adj.length);
+  let random3 = Math.floor(Math.random() * noun.length);
+  let random4 = Math.floor(Math.random() * extension.length);
+
+  //Concatenation of the random values into an excuse
+  document.querySelector("#excuse").innerHTML =
+    pronoun[random1] + adj[random2] + noun[random3] + extension[random4];
 };
